210e0ef83ccee4a2696ac3eb0fe3667ee7e188c8
Shia <rise.shia@gmail.com>
2022-07-19 19:04:18 +0900

    bundle gem affected_tests

---
+++ lib/affected_tests.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "affected_tests/version"
+
+module AffectedTests
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/affected_tests/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AffectedTests
+  VERSION = "0.1.0"
+end
